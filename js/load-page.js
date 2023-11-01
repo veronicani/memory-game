9b2b1a32cd24c2c758384badbae6919a131aa648
Veronica Ni <vni1294@gmail.com>
2023-11-01 14:05:35 -0400

    refactor load-page.js

--- js/load-page.js
+++ js/load-page.js
@@ -5,20 +5,37 @@ createLoadPage();
 function createLoadPage() {
   const body = document.querySelector('body');
   const loadPage = document.createElement('header');
+  loadPage.append(createTitle(), createBestScoreTxt(), createStartBtn(), createClearBtn());
+  body.append(loadPage);
+}
+
+function createTitle() {
   const title = document.createElement('h1');
   title.innerText = 'Memory Game';
+  return title;
+}
+
+function createBestScoreTxt() {
   const bestScoreTxt = document.createElement('h2');
-  bestScoreTxt.innerText = 'Best Score: ' + retrieveBestScore();
+  bestScoreTxt.innerText = `Best Score: ${retrieveBestScore()}`;
+  return bestScoreTxt;
+}
+
+function createStartBtn() {
   const startBtn = document.createElement('button');
   startBtn.innerText = "Start"; 
   startBtn.addEventListener('click', removeLoadPage);
+  return startBtn;
+}  
+
+function createClearBtn() {
   const clearBtn = document.createElement('button');
   clearBtn.classList.add('secondary-btn');
   clearBtn.innerText = "Clear Best Score";
   clearBtn.addEventListener('click', clearBestScore);
-  loadPage.append(title, bestScoreTxt, startBtn, clearBtn);
-  body.append(loadPage);
-}
+  return clearBtn;
+}  
+
 
 function removeLoadPage(evt) {
   setTimeout(function() {
@@ -29,35 +46,41 @@ function removeLoadPage(evt) {
 /* Retrieve best score from local storage */
 /*https://gamedevjs.com/articles/using-local-storage-for-high-scores-and-game-progress/*/
 
+
 function retrieveBestScore() {
-  //if there is a local storage
   if (localStorage) {
-    //retrieve the currentScore
-    const currentScore = localStorage.getItem('currentScore');
-    let bestScore = localStorage.getItem('bestScore');
-    //if there is a previous bestScore in local storage
+    const bestScore = localStorage.getItem('bestScore');  
     if (bestScore !== undefined) {
-      //if the currentScore is lower than the bestScore
-      if (currentScore < bestScore) {
-        //reassign the bestScore to be the currentScore;
-        bestScore = currentScore;
-        //store the updated bestScore in the local storage
-        localStorage.setItem('bestScore', bestScore);
-      }
-      //return bestScore;
-      return bestScore;
+      return updateBestScore();
     } else {
-      //store a bestScore in local cache with value of infinity;
-      localStorage.setItem('bestScore', +Infinity);
-      //return '--';
-      return '--';
+      return setDefaultBestScore();
     }
   }
 }
+
+function updateBestScore() {
+  const currentScore = localStorage.getItem('currentScore');
+  let currentBest = localStorage.getItem('bestScore');
+  console.log(localStorage); 
+  if (Number(currentScore) < Number(currentBest)) {      
+    currentBest = currentScore;      
+    localStorage.setItem('bestScore', currentBest);
+  }
+  return currentBest;   
+}
+
+ function setDefaultBestScore() {
+  localStorage.setItem('bestScore', +Infinity);
+  return '--';
+ }
+  
+
 /* Reset the best score */
 function clearBestScore() {
   //reassign best score in the local storage to be Infinity;
+  let bestScore = +Infinity;
   localStorage.setItem('bestScore', +Infinity);
   const bestScoreTxt = document.querySelector('header > h2');
   bestScoreTxt.innerText = 'Best Score: --';
+  return bestScore;
 }
