e8d498b9fb1808037e22f13cfdf25b48243dd28f
Veronica Ni <vni1294@gmail.com>
2023-11-02 12:13:29 -0400

    refactor memory-game.js

--- js/memory-game.js
+++ js/memory-game.js
@@ -21,7 +21,7 @@ function generateRandomColorShape(num) {
   for (let i = 0; i < num; i++) {
     const colorShape = [];
     //push the string of the hex code to the array
-    colorShape.push(generateHex().toString());
+    colorShape.push(generateHex());
     //push a random shape SVG to the array;
     colorShape.push(pickRandomShape());
     colorShapeHalf.push(colorShape);
@@ -39,9 +39,9 @@ function generateHex() {
   for (let i = 0; i < 6; i++) {
     hexCode += hexChars[Math.floor(Math.random() * 16)];
   }
-  return hexCode;
+  return hexCode.toString();
 }
-
+/** Pick random shape from array of shape SVGs */
 function pickRandomShape() {
   const SHAPES = {
     square: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96z"/></svg>',
@@ -72,7 +72,6 @@ function shuffle(items) {
     // swap item at i <-> item at j
     [items[i], items[j]] = [items[j], items[i]];
   }
-
   return items;
 }
 
